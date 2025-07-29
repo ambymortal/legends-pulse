5445cf54a3cdaa6dd65d9dd7c4ac5784474e0c49
Ambrose Frost <thefirstfrost@proton.me>
2025-07-28 22:34:09 -0400

    init bot test command that successfully shows response

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+const securityToken = ""
+
+func Init() {
+	discord, err := discordgo.New("Bot " + securityToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// event handlers
+	discord.AddHandler(newMessage)
+
+	// Open websocket
+	discord.Open()
+
+	// Run until process is terminated
+	fmt.Println("Bot running...")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	//close websocket
+	defer discord.Close()
+}
+
+func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
+
+	// Ignore bot messages
+	if message.Author.ID == discord.State.User.ID {
+		return
+	}
+
+	// test command
+	switch {
+	case message.Content == "~test":
+		discord.ChannelMessageSend(message.ChannelID, "response")
+	}
+}
